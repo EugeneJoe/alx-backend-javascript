0a709d98359011f6af409bbf21faa0d5ca087308
EugeneJoe <jmuthui70@gmail.com>
2021-11-28 18:49:07 +0000

    Update

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,9 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const res = [];
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.forEach((result) => res.push({ status: 'fulfilled', value: result })))
+    .catch((err) => res.push({ status: 'rejected', value: err.toString() }));
 }
