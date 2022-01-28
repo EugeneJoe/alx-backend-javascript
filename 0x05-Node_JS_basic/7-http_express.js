4e9fa8151bbba31858ea36062366645bc0105c1b
EugeneJoe <jmuthui70@gmail.com>
2022-01-28 09:39:23 +0000

    Fix errors

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -13,34 +13,35 @@ function countStudents(fileName) {
     readFile(fileName, (err, data) => {
       if (err) {
         reject(err);
-      }
-      let output = '';
-      const lines = data.toString().split('\n');
-      for (let i = 0; i < lines.length; i += 1) {
-        if (lines[i]) {
-          length += 1;
-          const field = lines[i].toString().split(',');
-          if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-            students[field[3]].push(field[0]);
-          } else {
-            students[field[3]] = [field[0]];
-          }
-          if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-            fields[field[3]] += 1;
-          } else {
-            fields[field[3]] = 1;
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
           }
         }
-      }
-      const l = length - 1;
-      output += `Number of students: ${l}\n`;
-      for (const [key, value] of Object.entries(fields)) {
-        if (key !== 'field') {
-          output += `Number of students in ${key}: ${value}. `;
-          output += `List: ${students[key].join(', ')}\n`;
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
         }
+        resolve(output);
       }
-      resolve(output);
     });
   });
 }
@@ -51,9 +52,9 @@ app.get('/', (request, response) => {
 app.get('/students', (request, response) => {
   countStudents(process.argv[2].toString()).then((output) => {
     const outString = output.slice(0, -1);
-    response.send(['This is the list of our students', outString].join('\n'));
+    response.send(['This is the list of our students', outString.join('\n')]);
   }).catch(() => {
-    response.send('Error: Cannot load the database');
+    response.send('This is the list of our students\nCannot load the database');
   });
 });
 
