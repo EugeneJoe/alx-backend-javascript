8976ecd3885aeded676ef27d4eed16bc8a46ebd8
EugeneJoe <jmuthui70@gmail.com>
2022-01-28 16:12:38 +0000

    Update

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,11 +1,12 @@
-const index = require("./routes/index");
+const router = require("./routes/index");
 const express = require("express");
 const app = express();
 const port = 1245;
 
-app.use(index);
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
 
-app.listen(port, () => {
-});
+app.listen(port);
 
 export default app;
